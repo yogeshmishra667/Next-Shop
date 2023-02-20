3eab6c2137e570eb9fde174f89e8290940c6cd39
yogesh mishra <yogeshmishra667@gmail.com>
2023-02-20 14:49:14 +0530

    fic bug

--- pages/profile.js
+++ pages/profile.js
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { getError } from '../utils/error';
 import axios from 'axios';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 
 export default function ProfileScreen() {
   const { data: session } = useSession();
